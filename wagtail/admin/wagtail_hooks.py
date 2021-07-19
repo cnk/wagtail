4e43a290d3d47b35dcdcadbd691aa9fd8894b158
Cynthia Kiser <cnk@caltech.edu>
2021-07-19 13:28:57 -0700

    Create new permission policy CollectionMangementPermissionPolicy
    Also updated tests, docs, and made a migration to move from model-level permissions to new granular collection management

--- wagtail/admin/wagtail_hooks.py
+++ wagtail/admin/wagtail_hooks.py
@@ -11,7 +11,6 @@ import wagtail.admin.rich_text.editors.draftail.features as draftail_features
 from wagtail import __version__
 from wagtail.admin.auth import user_has_any_page_permission
 from wagtail.admin.forms.collections import GroupCollectionManagementPermissionFormSet
-from wagtail.admin.localization import get_available_admin_languages, get_available_admin_time_zones
 from wagtail.admin.menu import MenuItem, SubmenuMenuItem, reports_menu, settings_menu
 from wagtail.admin.navigation import get_explorable_root_page
 from wagtail.admin.rich_text import (
